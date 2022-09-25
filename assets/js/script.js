706efd0853b681efe8e76d0955fe01d7034abc7f
simranyadav2300 <94699153+simranyadav2300@users.noreply.github.com>
2022-09-26 02:01:07 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["a Nutritionist", "an Athlete", "a Yogini","an Orator"],
+    strings: ["a Nutritionist", "an Athlete", "an Orator", "a Yogini"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
